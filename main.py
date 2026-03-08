3313075c498dba477973c72285546c00a07e4aee
kimhagyung <gkrud98657@gmail.com>
2026-03-08 12:54:07 +0900

    # 17.1 Email Graph

--- main.py
+++ main.py
@@ -1,6 +1,68 @@
-def main():
-    print("Hello from workflow-testing!")
+from typing import Literal, List
+from langgraph import graph
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END 
+from langgraph.checkpoint.memory import MemorySaver
 
+class EmailState(TypedDict):
+    email : str
+    category : Literal["spam","normal","urgent"]
+    priority_score : int
+    response : str
 
-if __name__ == "__main__":
-    main()
+# 노드 추가 
+
+# 이메일을 카테고리 별로 나누는 노드 
+def categorize_email(state: EmailState):
+    email = state["email"].lower()
+
+    if "urgent" in email or "assap" in email:
+        category = "urgent"
+    elif "offer" in email or "discount" in email:
+        category = "spam"
+    else: 
+        category = "normal"
+    return {
+        "category" : category
+    }
+
+# 우선순위 할당 노드 
+def assing_priority(state:EmailState):
+    scores = {
+        "urgent" : 10, 
+        "normal" : 5,
+        "spam" : 1,
+    }
+    return {
+        "priority_score" : scores[state["category"]],
+    }
+
+# 응답 
+def draft_response(state: EmailState) -> EmailState:
+    response = {
+        "urgent" : "I will answer you as fast as i can",
+        "normal" : "I'll get back to you soon",
+        "spam" : "Go away!" 
+    }
+    return {
+        "response" : response[state["category"]]
+    }
+
+graph_builder = StateGraph(EmailState)
+
+graph_builder.add_node("categorize_email", categorize_email)
+graph_builder.add_node("assing_priority", assing_priority)
+graph_builder.add_node("draft_response", draft_response)
+
+graph_builder.add_edge(START, "categorize_email")
+graph_builder.add_edge("categorize_email", "assing_priority")
+graph_builder.add_edge("assing_priority", "draft_response")
+graph_builder.add_edge("draft_response", END)
+
+graph = graph_builder.compile()
+
+# 테스트 
+#result = graph.invoke({"email" : "i need to talk to you urgently"})
+result = graph.invoke({"email" : "i have an offer for you"})
+
+print(result)
