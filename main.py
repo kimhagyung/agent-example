a117cf27a6523bb29dc272043cfacbab3787c9fc
kimhagyung <gkrud98657@gmail.com>
2026-03-22 14:23:47 +0900

    # 19.2 Feynman Agent_1

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ load_dotenv()
 
 from langgraph.graph import START, END, StateGraph, MessagesState
 from agent.classification_agent import classification_agent
+from agent.teacher_agent import teacher_agent
+from agent.feynman_agent import feynman_agent
 
 class TutorState(MessagesState):
     pass 
@@ -10,6 +12,8 @@ class TutorState(MessagesState):
 graph_builder = StateGraph(TutorState)
 
 graph_builder.add_node("classification_agent", classification_agent )
+graph_builder.add_node("teacher_agent", teacher_agent )
+graph_builder.add_node("feynman_agent", feynman_agent )
 
 graph_builder.add_edge(START, "classification_agent")
  
--- tools/shared_tools.py
+++ tools/shared_tools.py
@@ -1,8 +1,11 @@
-from langchain_core.tools import tool
+import re
+import os
 from langgraph.types import Command
+from langchain_core.tools import tool
+from firecrawl import FirecrawlApp, ScrapeOptions
 
 # 여기서 agent의 이름을 받고 그  agent로 전환 
-# 해당 tool이 서브 그래프에서 호출되면 부모 그래프에 있는 노드로 전환시켜 달라고 해야함. 
+# 해당 tool이 서브 그래프에서 호출되면 부모  그래프에 있는 노드로 전환시켜 달라고 해야함. 
 @tool
 def transfer_to_agent(agent_name : str):
     """
@@ -20,3 +23,46 @@ def transfer_to_agent(agent_name : str):
     #     graph=Command.PARENT
     # )
 
+@tool
+def web_search_tool(query: str):
+    """
+    Web Search Tool.
+    Args:
+        query: str
+            The query to search the web for.
+    Returns
+        A list of search results with the website content in Markdown format.
+    """
+    app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+
+    response = app.search(
+        query=query,
+        limit=5,
+        scrape_options=ScrapeOptions(
+            formats=["markdown"],
+        ),
+    )
+
+    if not response.success:
+        return "Error using tool."
+
+    cleaned_chunks = []
+
+    for result in response.data:
+
+        title = result["title"]
+        url = result["url"]
+        markdown = result["markdown"]
+
+        cleaned = re.sub(r"\\+|\n+", "", markdown).strip()
+        cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
+
+        cleaned_result = {
+            "title": title,
+            "url": url,
+            "markdown": cleaned,
+        }
+
+        cleaned_chunks.append(cleaned_result)
+
+    return cleaned_chunks
