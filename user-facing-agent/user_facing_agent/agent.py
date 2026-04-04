b167e345a89dd4bd3eff72f079c4791b157e97bb
kimhagyung <gkrud98657@gmail.com>
2026-04-04 22:15:55 +0900

    #  20.3 RemteA2aAgent

--- user-facing-agent/user_facing_agent/agent.py
+++ user-facing-agent/user_facing_agent/agent.py
@@ -3,12 +3,22 @@ load_dotenv()
 
 from google.adk.agents import Agent 
 from google.adk.models.lite_llm import LiteLlm
+from google.adk.agents.remote_a2a_agent import (
+    RemoteA2aAgent,
+    AGENT_CARD_WELL_KNOWN_PATH
+)
+
+history_agent = RemoteA2aAgent(
+    name ="HistoryHelperAgent",
+    description="An agent that can help  students with history homework", 
+    agent_card = f"http://127.0.0.1:8001/{AGENT_CARD_WELL_KNOWN_PATH}" # agent card의 url 정해야함 
+)
 
 root_agent = Agent(
     name = "StudentHelperAgent",
     description="An agent that can help students with their homework",
     model = LiteLlm("openai/gpt-4o"),
     sub_agents=[ # 나중에 adk로 만든 다른 port에서 실행될 romte agent가 들어감 
-
+        history_agent
     ]
 )
