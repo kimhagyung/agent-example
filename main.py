4ae99fc97b43570cf7dabe2a87d3b61ac1a499ac
kimhagyung <gkrud98657@gmail.com>
2026-04-12 19:24:25 +0900

    #21.3 StreamingResponse

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from openai import AsyncOpenAI 
 from pydantic import BaseModel
 
@@ -40,3 +41,34 @@ async def create_message(conversation_id : str, message_input : CreateMessageInp
     return {
         'answer' : answer.final_output
     }
+
+@app.post("/conversations/{conversation_id}/message-stream")
+async def create_message(conversation_id : str, message_input : CreateMessageInput) -> CreateMessageOutput:
+    async def event_generator(): 
+        events =  Runner.run_streamed(
+            starting_agent=agent, 
+            input = message_input.question, 
+            conversation_id=conversation_id,
+        ) 
+        async for event in events.stream_events():
+            if event.type == "raw_response_event" and event.data.type == "response.output_text.delta":
+                yield event.data.delta # yield를 쓰면 함수를 죽이지 않고 이 값을 함수 밖으로 뱉어낸다.(return을 쓰면 첫번째 함수에서 죽어버림)
+                # 유저에게 보내고 싶은 이벤트를 yield로 보낸 
+
+    return StreamingResponse(event_generator(), media_type="text/plain")
+ 
+# 에이전트가 하는 모든것을 스트리밍 한다. 
+@app.post("/conversations/{conversation_id}/message-stream-all")
+async def create_message_all(conversation_id : str, message_input : CreateMessageInput) -> CreateMessageOutput:
+    async def event_generator(): 
+        events =  Runner.run_streamed(
+            starting_agent=agent, 
+            input = message_input.question, 
+            conversation_id=conversation_id,
+        ) 
+        async for event in events.stream_events():
+            if event.type == "raw_response_event":
+                yield f"{event.data.to_json()}\n"
+
+    return StreamingResponse(event_generator(), media_type="text/plain")
+ 
